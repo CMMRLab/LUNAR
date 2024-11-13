82066948c224f25f7cec9e2fbefd31ede37be2f2
Josh Kemppainen <jdkemppa@mtu.edu>
2024-11-13 12:49:33 -0500

    LUNAR update 13Noveber2024

--- all2lmp.py
+++ all2lmp.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 @author: Josh Kemppainen
-Revision 1.20
-October 4th, 2024
+Revision 1.21
+November 13th, 2024
 Michigan Technological University
 1400 Townsend Dr.
 Houghton, MI 49931
