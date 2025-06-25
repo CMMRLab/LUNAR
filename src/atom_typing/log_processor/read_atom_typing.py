d213db6c942a6e5d89b0a864a0c7e04eca04d3b3
Josh Kemppainen <jdkemppa@mtu.edu>
2025-06-25 11:14:02 -0400

    LUNAR version 25June2025

--- src/atom_typing/log_processor/read_atom_typing.py
+++ src/atom_typing/log_processor/read_atom_typing.py
@@ -147,7 +147,9 @@ class logfile:
         # Go through and sum up all hybridizations to get system mass
         if self.hybridizations:
             for hybrid in self.hybridizations:
-                self.mass += self.hybridizations[hybrid]['Mass']
+                if hybrid.startswith('all-'):
+                    self.mass += self.hybridizations[hybrid]['Mass']
+
                 
     ################################
     # Define methods to this class #
@@ -201,132 +203,132 @@ if __name__ == "__main__":
     print('Elements list: ', log.elements)
     print('System mass  : ', log.mass)
     
-    # Show how to access Table: Cluster Data
-    if log.clusters:
-        print('\nCluster analysis')
-        for molID in log.clusters:
-            print('{} -> {}'.format(molID, log.clusters[molID]))
+    # # Show how to access Table: Cluster Data
+    # if log.clusters:
+    #     print('\nCluster analysis')
+    #     for molID in log.clusters:
+    #         print('{} -> {}'.format(molID, log.clusters[molID]))
         
-    # Show how to access Table: Bond Order Data
-    if log.bond_order:
-        print('\nBond Order')
-        for bond_elements in log.bond_order:
-            print('{} -> {}'.format(bond_elements, log.bond_order[bond_elements]))
+    # # Show how to access Table: Bond Order Data
+    # if log.bond_order:
+    #     print('\nBond Order')
+    #     for bond_elements in log.bond_order:
+    #         print('{} -> {}'.format(bond_elements, log.bond_order[bond_elements]))
         
-    # Show how to access Table: Atom Bond Order Data
-    if log.atom_bond_order:
-        print('\nAtom Bond Order')
-        for element in log.atom_bond_order:
-            print('{} -> {}'.format(element, log.atom_bond_order[element]))
+    # # Show how to access Table: Atom Bond Order Data
+    # if log.atom_bond_order:
+    #     print('\nAtom Bond Order')
+    #     for element in log.atom_bond_order:
+    #         print('{} -> {}'.format(element, log.atom_bond_order[element]))
             
-    # Show how to access Table: Bond Length Data (if bonds_via_distance is used)
-    if log.bond_length:
-        print('\nBond Length')
-        for bond_elements in log.bond_length:
-            print('{} -> {}'.format(bond_elements, log.bond_length[bond_elements]))
+    # # Show how to access Table: Bond Length Data (if bonds_via_distance is used)
+    # if log.bond_length:
+    #     print('\nBond Length')
+    #     for bond_elements in log.bond_length:
+    #         print('{} -> {}'.format(bond_elements, log.bond_length[bond_elements]))
         
-    # Show how to access Table: Hybridization Data
-    if log.hybridizations:
-        print('\nHybridization analysis')
-        for hybrid in log.hybridizations:
-            print('{} -> {}'.format(hybrid, log.hybridizations[hybrid]))
+    # # Show how to access Table: Hybridization Data
+    # if log.hybridizations:
+    #     print('\nHybridization analysis')
+    #     for hybrid in log.hybridizations:
+    #         print('{} -> {}'.format(hybrid, log.hybridizations[hybrid]))
         
-    # Show how to access Table: Ring Data
-    if log.rings:
-        print('\nRing analysis')
-        for ring in log.rings:
-            print('{} -> {}'.format(ring, log.rings[ring]))
+    # # Show how to access Table: Ring Data
+    # if log.rings:
+    #     print('\nRing analysis')
+    #     for ring in log.rings:
+    #         print('{} -> {}'.format(ring, log.rings[ring]))
         
-        print('\nMANUAL Ring analysis')
-        print("log.rings[6]['C']['%Mass']", log.rings[6]['C']['%Mass'])
+    #     print('\nMANUAL Ring analysis')
+    #     print("log.rings[6]['C']['%Mass']", log.rings[6]['C']['%Mass'])
             
-    # Show how to access Table: Atom Type Tally
-    if log.atom_types_tally:
-        print('\nAtom Types Tally')
-        for ring in log.atom_types_tally:
-            print('{} -> {}'.format(ring, log.atom_types_tally[ring]))
+    # # Show how to access Table: Atom Type Tally
+    # if log.atom_types_tally:
+    #     print('\nAtom Types Tally')
+    #     for ring in log.atom_types_tally:
+    #         print('{} -> {}'.format(ring, log.atom_types_tally[ring]))
             
-    # Show how to access Table: By Products Tally
-    if log.by_products_tally:
-        print('\nBy Products Tally')
-        for by_product in log.by_products_tally:
-            print('{} -> {}'.format(by_product, log.by_products_tally[by_product]))
+    # # Show how to access Table: By Products Tally
+    # if log.by_products_tally:
+    #     print('\nBy Products Tally')
+    #     for by_product in log.by_products_tally:
+    #         print('{} -> {}'.format(by_product, log.by_products_tally[by_product]))
             
-    # Show how to access Table: Ringed Cluster Data
-    if log.ringed_clusters:
-        print('\nRinged Clusters')
-        for ring_type in log.ringed_clusters:
-            print('{} -> {}'.format(ring_type, log.ringed_clusters[ring_type]))
+    # # Show how to access Table: Ringed Cluster Data
+    # if log.ringed_clusters:
+    #     print('\nRinged Clusters')
+    #     for ring_type in log.ringed_clusters:
+    #         print('{} -> {}'.format(ring_type, log.ringed_clusters[ring_type]))
             
-    # Show how to access Table: Fused Ring Cluster Data
-    if log.fused_ringed_clusters:
-        print('\nFused Ringed Clusters')
-        for fusedID in log.fused_ringed_clusters:
-            print('{} -> {}'.format(fusedID, log.fused_ringed_clusters[fusedID]))
+    # # Show how to access Table: Fused Ring Cluster Data
+    # if log.fused_ringed_clusters:
+    #     print('\nFused Ringed Clusters')
+    #     for fusedID in log.fused_ringed_clusters:
+    #         print('{} -> {}'.format(fusedID, log.fused_ringed_clusters[fusedID]))
             
-    # Try getting all table values in string format for
-    # atom_typing_log_processor.py logger = 'all-table:TABLE-NAME'.
-    def table_expand(table, dict_name):
-        all_table = []
-        for valueID0 in table:
-            for valueID1 in table[valueID0]:
-                # ring's dictionary may be nested  "3-deep"
-                if isinstance(table[valueID0][valueID1], dict):
-                    for valueID2 in table[valueID0][valueID1]:
-                        if isinstance(valueID0, str):
-                            index0 = "'{}'".format(valueID0)
-                        else: index0 = "{}".format(valueID0)
-                        if isinstance(valueID1, str):
-                            index1 = "'{}'".format(valueID1)
-                        else: index1 = "{}".format(valueID1)
-                        if isinstance(valueID2, str):
-                            index2 = "'{}'".format(valueID2)
-                        else: index2 = "{}".format(valueID2)
-                        all_table.append("{}[{}][{}][{}]".format(dict_name, index0, index1, index2))
-                else: # every other dictionary will be nested "2-deep"
-                    if isinstance(valueID0, str):
-                        index0 = "'{}'".format(valueID0)
-                    else: index0 = "{}".format(valueID0)
-                    if isinstance(valueID1, str):
-                        index1 = "'{}'".format(valueID1)
-                    else: index1 = "{}".format(valueID1)
-                    all_table.append("{}[{}][{}]".format(dict_name, index0, index1))
-        return all_table
+    # # Try getting all table values in string format for
+    # # atom_typing_log_processor.py logger = 'all-table:TABLE-NAME'.
+    # def table_expand(table, dict_name):
+    #     all_table = []
+    #     for valueID0 in table:
+    #         for valueID1 in table[valueID0]:
+    #             # ring's dictionary may be nested  "3-deep"
+    #             if isinstance(table[valueID0][valueID1], dict):
+    #                 for valueID2 in table[valueID0][valueID1]:
+    #                     if isinstance(valueID0, str):
+    #                         index0 = "'{}'".format(valueID0)
+    #                     else: index0 = "{}".format(valueID0)
+    #                     if isinstance(valueID1, str):
+    #                         index1 = "'{}'".format(valueID1)
+    #                     else: index1 = "{}".format(valueID1)
+    #                     if isinstance(valueID2, str):
+    #                         index2 = "'{}'".format(valueID2)
+    #                     else: index2 = "{}".format(valueID2)
+    #                     all_table.append("{}[{}][{}][{}]".format(dict_name, index0, index1, index2))
+    #             else: # every other dictionary will be nested "2-deep"
+    #                 if isinstance(valueID0, str):
+    #                     index0 = "'{}'".format(valueID0)
+    #                 else: index0 = "{}".format(valueID0)
+    #                 if isinstance(valueID1, str):
+    #                     index1 = "'{}'".format(valueID1)
+    #                 else: index1 = "{}".format(valueID1)
+    #                 all_table.append("{}[{}][{}]".format(dict_name, index0, index1))
+    #     return all_table
     
-    print('\n\nTesting all_table: hybridizations')
-    all_table = table_expand(log.hybridizations, 'hybridizations')
-    print(all_table[:10])
-    
-    print('\n\nTesting all_table: bond_order')
-    all_table = table_expand(log.bond_order, 'bond_order')
-    print(all_table[:10])
-    
-    print('\n\nTesting all_table: ringed_clusters')
-    all_table = table_expand(log.ringed_clusters, 'ringed_clusters')
-    print(all_table[:10])
-    
-    print('\n\nTesting all_table: clusters')
-    all_table = table_expand(log.clusters, 'clusters')
-    print(all_table[:10])
-
-    print('\n\nTesting all_table: rings')
-    all_table = table_expand(log.rings, 'rings')
-    print(all_table[:10])
-    
-    # print('\nMANUAL Ring analysis')
-    # print("log.rings[3]['Count']", log.rings[3]['Count'])
-    # print("log.rings[3]['C']['%Mass']", log.rings[3]['C']['%Mass'])
-    
-    # print('\n\nTesting all_table: atom_types_tally')
-    # all_table = table_expand(log.atom_types_tally, 'atom_types_tally')
+    # print('\n\nTesting all_table: hybridizations')
+    # all_table = table_expand(log.hybridizations, 'hybridizations')
     # print(all_table[:10])
     
-    print('\n\nTesting all_table: clusters')
-    all_table = table_expand(log.clusters, 'clusters')
-    print(all_table[:10])
+    # print('\n\nTesting all_table: bond_order')
+    # all_table = table_expand(log.bond_order, 'bond_order')
+    # print(all_table[:10])
     
-    print('\nMANUAL Ring analysis')
-    print("clusters[1]['Molecule Formula']['3']", log.clusters[1]['Molecule Formula'])
+    # print('\n\nTesting all_table: ringed_clusters')
+    # all_table = table_expand(log.ringed_clusters, 'ringed_clusters')
+    # print(all_table[:10])
+    
+    # print('\n\nTesting all_table: clusters')
+    # all_table = table_expand(log.clusters, 'clusters')
+    # print(all_table[:10])
+
+    # print('\n\nTesting all_table: rings')
+    # all_table = table_expand(log.rings, 'rings')
+    # print(all_table[:10])
+    
+    # # print('\nMANUAL Ring analysis')
+    # # print("log.rings[3]['Count']", log.rings[3]['Count'])
+    # # print("log.rings[3]['C']['%Mass']", log.rings[3]['C']['%Mass'])
+    
+    # # print('\n\nTesting all_table: atom_types_tally')
+    # # all_table = table_expand(log.atom_types_tally, 'atom_types_tally')
+    # # print(all_table[:10])
+    
+    # print('\n\nTesting all_table: clusters')
+    # all_table = table_expand(log.clusters, 'clusters')
+    # print(all_table[:10])
+    
+    # print('\nMANUAL Ring analysis')
+    # print("clusters[1]['Molecule Formula']['3']", log.clusters[1]['Molecule Formula'])
     
         
         
